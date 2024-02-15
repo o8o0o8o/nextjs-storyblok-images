06ad6c0f7f04aee57d3369ebf03a69715d283927
Sergey Labut <sergeylabut@gmail.com>
2024-02-15 19:01:46 +0100

    Try different sizes

---
+++ app/page10/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+
+export default function CLSPage() {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        background: "black",
+      }}
+    >
+      <div style={{ position: "relative", width: "100px", height: "100px" }}>
+        <Image
+          src="/hero-bg-mobile.gif"
+          alt="Some Image"
+          fill
+          objectFit="cover"
+        />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          color: "white",
+          top: "50%",
+          left: "50%",
+          fontSize: "24px",
+        }}
+      >
+        Code, Career, Creativity - the contemporary software engineering and
+        design festival
+      </div>
+    </div>
+  );
+}
